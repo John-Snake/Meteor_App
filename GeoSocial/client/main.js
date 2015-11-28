11f0b8ef5d6352cbd7235fa9b338c430750918c7
Snake <fabio-canta@hotmail.it>
2015-11-28 21:35:01 +0100

    Routes and Layout

--- GeoSocial/client/main.js
+++ GeoSocial/client/main.js
@@ -1,23 +1,12 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+/* Imposto il template principale */
+Router.route('/', {
+    template: 'home',
+});
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+/* Imposto il layout generale di tutte le routes del progetto */
+Router.configure({
+    layoutTemplate: 'layout'
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Router.route('/register');
+Router.route('/login');
